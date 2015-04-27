d6b324e228595ae34e54fe3c736c3816893921b4
ybrbnf <ybrbnf418@gmail.com>
2015-04-27 12:55:29 +0300

    update

--- p1-1.py
+++ p1-1.py
@@ -1,43 +1,37 @@
 #!/usr/bin/python
-
 from math import sqrt
 def multipliers(n): #1.a
  a=int(sqrt(n))
- b=[]
+ multipliers_of_n=[]
  for i in range (1, a):
      if (n%i)==0:
-         b.append(i)
+         multipliers_of_n.append(i)
          n=(n//i)
- return b
-
+ return multipliers_of_n
 def equation(a,b,c): #1.b
- d=0
- i=1
- d=b**2-4*a*c
- if d<0:
+ #ax^2+bx+c=0
+ if b**2-4*a*c<0:
      return 'deistvitelnyh korney net'
  else:
-     return round((-b+sqrt(d))/2*a, 2), round((-b-sqrt(d))/2*a, 2)
-
+     return round((-b+sqrt(b**2-4*a*c))/2*a, 2), round((-b-sqrt(b**2-4*a*c))/2*a, 2)
 def simple(a): #1.c
- x=0
+ counter=0
  for i in range(1, a):
      if a%i==0:
-         x+=1
- if x>1:
+         counter+=1
+ if counter>1:
      return a, 'isnt simple'
  else:
      return a,'is simple'
-
-def atm(c): #1.d
+def atm(summ): #1.d
  b=0
  i=0
  q=''
- for a in 100, 50, 20, 10, 5, 1:
-     b=c//a
-     c=c-(c//a)*a
+ for nominal in 100, 50, 20, 10, 5, 1:
+     b=summ//nominal
+     summ=summ-(summ//nominal)*nominal
      i+=b
-     print b, 'banknot po',a, 'deneg'
+     print b, 'banknot po',nominal, 'deneg'
  return 'vsego', i, 'banknot'
 print multipliers(30030)
 print equation(2, 19, 35)
--- p1-2.py
+++ p1-2.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python
 import random
-
-def letters(s):
- up=s.upper()
- q=[]
- for i in range (len(s)):
-     if up[i]==s[i] and s[i]!=' ':
-         q.append(s[i])
- return q
- 
-def palindrome(s):
- l=s.lower().replace(' ', '')
- w=l[::-1]
- if w==l: return 'palindrome'
+def letters(string):
+ up_string=string.upper()
+ up_letter=[]
+ for i in range (len(string)):
+     if up_string[i]==string[i] and string[i]!=' ':
+         up_letter.append(string[i])
+ return up_letter
+def palindrome(string):
+ lower_string=string.lower().replace(' ', '')
+ reverce_lower_string=lower_string[::-1]
+ if reverce_lower_string==lower_string: return 'palindrome'
  else: return 'isnt palindrome'
-
-def find_letter(s, letter):
- l=s.split()
- d=[]
- for i in range(len(l)):
-    if l[i].startswith(letter):
-        d.append(l[i])
- return d
-
-def mix_words(s):
- w=s.split(' ')
- random.shuffle(w)
- q=' '.join(w)
- e=q.capitalize()
- return e
-
-
+def find_letter(string_in, letter):
+ splitted_string=string_in.split()
+ string_out=[]
+ for i in range(len(splitted_string)):
+    if splitted_string[i].startswith(letter):
+        string_out.append(splitted_string[i])
+ return string_out
+def mix_words(string_in):
+ splitted_string=string_in.split(' ')
+ random.shuffle(splitted_string)
+ joined_string=' '.join(splitted_string)
+ string_out=joined_string.capitalize()
+ return string_out
 print letters("Trees Are So Kind")
-
 print palindrome("A roza upala na lapu Azora")
-
 print find_letter("Bears are the best animals ever", 'a')
-
 print mix_words("Bears are the best animals ever")
