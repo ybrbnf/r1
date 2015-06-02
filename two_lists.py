015bac1e6be78674b111c475e51837ce2b2ed436
ybrbnf <ybrbnf418@gmail.com>
2015-06-02 12:34:58 +0300

    генератор из 2-х списков

--- two_lists.py
+++ two_lists.py
@@ -13,8 +13,8 @@ def func(list_1, list_2):
  for index_1 in range(len(lower_bound)):
     for index_2 in range(len(upper_bound)):
         if index_1==index_2==count:
-            count+=step
+            count+=step+1
             interval=(lower_bound[index_1],upper_bound[index_2])
             list_out.append(interval)
  return list_out
-print func([100,200,300], [0.1,0.2,0.3])
+print func([100,200,300,], [0.1,0.2,0.3])
