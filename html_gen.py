6cffd53a2d97c74689fc0f82ba96e2d4710a7cf1
ybrbnf <ybrbnf418@gmail.com>
2015-06-24 09:56:27 +0300

    ver 3.0

--- html_gen.py
+++ html_gen.py
@@ -1,50 +1,45 @@
 #!/usr/bin/python
 #coding: utf-8
+
 class Tree:
-    def __init__(self, data=None, parent=None, space=None):
+    def __init__(self, data=None, level=None):
         self.data=data
-        self.parent=parent
-        self.space=space
+        self.level=level
 
     def __str__(self):
         return str(self.data)
+        
 
-in_file='in.txt'#исходный файл
-out_file='out.html'#конечный файл
 open_tags=[]#список открывающах тегов
 close_tags=[]#список закравающ тегов
 tmp=[] #временный список
-counter=0
-with open(in_file, 'r') as read_file:# пишем теги из файла в списки 
-    for line in read_file:
+level=0
+        
+print ('Программа генерирует HTML файл (out.html) исходя из тегов, записанных построчно в исходном файле (in.txt)')
+try:
+    in_file=open('in.txt', 'r')
+except IOError:
+    print "Исходный файл не найден. Программа будет завершена."
+else:
+# пишем теги из файла в списки 
+    for line in in_file:
         open_tags.append(line.strip('\n'))
         tmp.append('/'+line.strip('\n'))
-close_tags=tmp[::-1]
-out_file=open('out.html', 'w')
-for element in open_tags:
-    if counter==0:
-        space=0
-        root=Tree(element,None,space)
-        out_file.write (' '*space+'<{}>\n'.format(element))
-        counter+=1
-        
-    elif counter!=0:
-        space=counter
-        node=Tree(element,root,space)
-        out_file.write (' '*space+'<{}>\n'.format(element))
-        counter+=1
-counter=0
-for element in close_tags:
-    if counter==0:
-        root=Tree(element,None,space)
-        out_file.write (' '*space+'<{}>\n'.format(element))
-        counter+=1
-        
-    elif counter!=0:
-        node=Tree(element,root,space)
-        out_file.write (' '*space+'<{}>\n'.format(element))
-        counter+=1
-        space=space-counter
-        
+    close_tags=tmp[::-1]
+    out_file=open('out.html', 'w')
 
-out_file.close()
+# строим дерево открывающих тегов
+    for elem in open_tags:
+        data=elem
+        node=Tree(data, level)
+        out_file.writelines (' '*level+'<'+str(node)+'>\n')
+        level+=1
+    level-=1
+
+# строим дерево закрывающих тегов
+    for elem in close_tags:
+        data=elem
+        node=Tree(data, level)
+        out_file.writelines (' '*level+'<'+str(node)+'>\n')
+        level-=1
+    print "Исходный файл обработан. Итоговый файл {}".format(out_file.name)
